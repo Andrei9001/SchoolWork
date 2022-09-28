064f8802c3de077262ae1141e503a4b20346355e
Andrejs A <113526422+Andrei9001@users.noreply.github.com>
2022-09-28 22:54:27 +0300

    gatavs pirmais apgalvojums

--- slinkais_astrologs/main.py
+++ slinkais_astrologs/main.py
@@ -7,13 +7,26 @@ dir = os.getcwd()
 path0 = "\slinkais_astrologs\ teikumi.txt"
 dienas = ["Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena", "Svētdiena"]
 apgalv = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-Divd = []   #delete?
+apg1 = []   #delete?
 sk =[]
-#===============================================
+diap = [0,19,22,99]
 
+#===============================================
+def teikums(x, y):
+    while len(sk) < 2:  #nejausi izvelas liniju
+        num = random.randint(x,y)
+        if num in sk:   #parbauda
+            continue
+        else: 
+            sk.append(num)  #pievieno izlieototo sarakstam
+            apg1.append(lasit[num])
+            teikums(diap[2],diap[3])
 
 #Darbības
 path = dir+path0.replace(" ","")
+with open(path, "r") as f:
+    lasit = f.readlines()
+
 
 #for later?
 '''with open(path, 'r') as f:
@@ -24,12 +37,7 @@ path = dir+path0.replace(" ","")
             break
 print(Divd)'''
 
-
-while len(sk) < 2:
-    num = random.randint(1,20)
-    if num in sk:
-        continue
-    else: 
-        sk.append(num)
+teikums(diap[0], diap[1])
 
 print(sk)
+print(apg1)
