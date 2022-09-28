a5cab77fbee0d7b88bc438945c05d3f0db294508
Andrejs A <113526422+Andrei9001@users.noreply.github.com>
2022-09-28 15:40:08 +0300

    changing folders

--- slinkais_astrologs/main.py
+++ slinkais_astrologs/main.py
@@ -1,4 +1,22 @@
 import math
+import random
+
+#Globālie mainīgie
+dienas = ["Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena", "Svētdiena"]
+d = 0
 
 def nedelas_horoskopi():
     return
+
+
+
+'''for i in dienas:
+    print(dienas[d])
+    d += 1'''
+    
+with open('teikumi.txt') as f:
+    lines = f.readlines()
+    print(lines, "end=\n")
+
+
+
