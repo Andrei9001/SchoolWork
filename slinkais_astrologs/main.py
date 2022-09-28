657e738dc4ffd0c68bcd61eec913c2bc7a64ebea
Andrejs A <113526422+Andrei9001@users.noreply.github.com>
2022-09-28 22:35:58 +0300

    continue

--- slinkais_astrologs/main.py
+++ slinkais_astrologs/main.py
@@ -1,30 +1,35 @@
-import math
+import os
+import os.path
 import random
 
-#Globālie mainīgie
+#Globālie mainīgie=============================
+dir = os.getcwd()
+path0 = "\slinkais_astrologs\ teikumi.txt"
 dienas = ["Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena", "Svētdiena"]
-d = 0
 apgalv = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-Divd = []
+Divd = []   #delete?
+sk =[]
+#===============================================
 
 
+#Darbības
+path = dir+path0.replace(" ","")
 
-
-
-def nedelas_horoskopi():
-    return
-
-with open('teikumi.txt', "r") as f:
-    teik = f.readlines()
-print(teik)
-
-with open('teikumi.txt', "r") as f:
+#for later?
+'''with open(path, 'r') as f:
     for i, rinda in enumerate(f):
         if i in apgalv:
             Divd.append(rinda.strip())
         elif i > 10:
             break
-print(Divd)
+print(Divd)'''
 
 
+while len(sk) < 2:
+    num = random.randint(1,20)
+    if num in sk:
+        continue
+    else: 
+        sk.append(num)
 
+print(sk)
