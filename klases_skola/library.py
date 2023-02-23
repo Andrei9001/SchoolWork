fd9e9eb7b5f5d3b504a746e08bc22adebad47bab
Andrejs A <113526422+Andrei9001@users.noreply.github.com>
2023-02-23 10:34:27 +0200

    tkinter hell

--- klases_skola/library.py
+++ klases_skola/library.py
@@ -1,7 +1,7 @@
 from PIL import Image
-
+from tkinter import messagebox as tkms
 from tkinter import filedialog as fd
-from tkinter import ttk
+
 
 class Photo:
     # bilžu apstrāde
@@ -9,30 +9,44 @@ class Photo:
         self.datne = datne
     def bilde(self):
         with Image.open(self.datne) as im:
-            print(self.datne, im.format, f"{im.size}x{im.mode}")
-            a=(100,200,300,400,50,10)
-            izmers = (a[2],a[2])
-            im.thumbnail(izmers)
+            tkms.showinfo(message='Lūdzu izvēlieties jaunā attēla saglabāšanas datni')
             map = fd.asksaveasfile(defaultextension='.jpg' ,filetypes=[('jpg','*.jpg')])
+            print(self.datne, im.format, f"{im.size}x{im.mode}")
+            izmers = (100,100)
+            im.thumbnail(izmers)
             im.save(map.name, im.format)
             im.show()
+
     def rotate(self):
         with Image.open(self.datne) as im:
+            tkms.showinfo(message='Lūdzu izvēlieties jaunā attēla saglabāšanas datni')
             map = fd.asksaveasfile(defaultextension='.jpg' ,filetypes=[('jpg','*.jpg')])
             im.rotate(90,expand=True).save(map.name, im.format)
     
     def thumb(self):
         with Image.open(self.datne) as im:
             print(self.datne, im.format, f"{im.size}x{im.mode}")
-            izmers = (self.a,self.b)
-            im.thumbnail(izmers)
+            tkms.showinfo(message='Lūdzu izvēlieties jaunā attēla saglabāšanas datni')
             map = fd.asksaveasfile(defaultextension='.jpg' ,filetypes=[('jpg','*.jpg')])
+            tkms.showinfo(message='Lūdzu apskatiet konsoli')
+            px = int(input('Vēlamais attēla kvadrāta izmērs pikseļos (px): '))
+            izmers = (px,px)
+            im.thumbnail(izmers)
             im.save(map.name, im.format)
             im.show()
     #def rot(self):
         #with Image.open(self.datne) as im:
 
+#==============variables=========================
+options = ['Attēla izmērs 100x100px','Attēla pagriešana par 90 grādiem','Attēla izveide ar pašizvēlētu izmēru']
 
+
+tkms.showinfo(message='Lūdzu izvēlieties attēlu')
 file = fd.askopenfilename(filetypes=[('jpg','*.jpg')])
 pic = Photo(file)
+
 pic.bilde()
+pic.rotate()
+pic.thumb()
+tkms.showerror(message='Kļūda! Visdrīzāk no nepareizi ievadītas izvēles')
+    
