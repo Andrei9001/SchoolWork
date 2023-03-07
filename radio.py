124d2df52282a05682d816e402869fe875d06f01
Andrejs A <113526422+Andrei9001@users.noreply.github.com>
2023-03-08 00:32:20 +0200

    radio

--- radio.py
+++ radio.py
@@ -4,4 +4,26 @@ import matplotlib.pyplot as plt
 from unicodedata import normalize
 
 table_radio = pd.read_html('https://www.vases.lv/lv/broadcasting_fm')
+frex =[]
 
+df = table_radio[0]
+df.head()
+
+
+frey = df['Frekv., MHz'].values.tolist()
+frey.sort()
+print(frey)
+freq = set(frey)
+
+
+for i in range(875,1080):
+    i = i/10
+    frex.append(i)
+
+for j in freq:
+    if j in frex:
+        frex.remove(j)
+    else: continue
+    
+print('Aiznemtās frekvences:\n{}\n'.format(freq))
+print('Brīvās frekvences:\n{}'.format(frex))
